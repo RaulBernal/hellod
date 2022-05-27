62ff041bbf9353b9233af00c75000781034dc2a5
Raul Bernal <bernal.raul@gmail.com>
2022-05-27 09:08:48 +0200

    ruderalis comment un-used

--- app/app.go
+++ app/app.go
@@ -16,7 +16,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/server/config"
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
 	"github.com/cosmos/cosmos-sdk/simapp"
-	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	//storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/version"
